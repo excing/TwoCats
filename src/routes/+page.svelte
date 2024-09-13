31172110cb0d678e518513a649b47b465b1cae89
chshqiang <courage.exc@gmail.com>
2024-09-13 20:32:42 +0800

    Update: input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,75 @@
 <script lang="ts">
-
 </script>
+
 <title>🐈2Cats🐱 —— 一个多语言学习 APP</title>
+<div class="flex h-full	">
+	<div class="flex-1"></div>
+	<div style="width: 680px;" class="flex flex-col h-full	">
+    <div class="flex-1">Hello</div>
+		<div class="flex items-end my-4">
+      <button type='button' class="btn-icon variant-filled-secondary mx-1">🛠</button>
+			<div
+				class="enterSomeContentStyle textarea"
+				contenteditable="true"
+				placeholder="Enter some long form content."
+			></div>
+      <button type='button' class="btn-icon variant-filled-secondary mx-1">OK</button>
+		</div>
+	</div>
+	<div class="flex-1"></div>
+</div>
+
+<style>
+	.enterSomeContentStyle {
+		background-color: rgb(var(--color-surface-700));
+		border-color: rgb(var(--color-surface-500));
+		border-radius: var(--theme-rounded-container);
+		width: 100%;
+    height: auto;
+    max-height: 140px;
+    overflow: auto;
+		transition-property:
+			color,
+			background-color,
+			border-color,
+			text-decoration-color,
+			fill,
+			stroke,
+			opacity,
+			box-shadow,
+			transform,
+			filter,
+			backdrop-filter,
+			-webkit-backdrop-filter;
+		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+		transition-duration: 200ms;
+		border-width: var(--theme-border-base);
+		padding-top: 0.5rem;
+		padding-right: 0.75rem;
+		padding-bottom: 0.5rem;
+		padding-left: 0.75rem;
+		font-size: 1rem;
+		line-height: 1.5rem;
+		--tw-shadow: 0 0 #0000;
+		appearance: none;
+		resize: vertical;
+		font-family: inherit;
+		font-feature-settings: inherit;
+		font-variation-settings: inherit;
+		font-weight: inherit;
+		letter-spacing: inherit;
+		color: inherit;
+		margin: 0;
+		--tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width)
+			var(--tw-ring-offset-color) !important;
+		--tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(0px + var(--tw-ring-offset-width))
+			var(--tw-ring-color) !important;
+		box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000) !important;
+		outline: none;
+	}
+
+	.enterSomeContentStyle:empty::before {
+		content: attr(placeholder);
+    color: rgb(201, 201, 201);
+	}
+</style>
